4d7e41e65ef8768c5e29ce50e3aa2801a0e3533f
AnshPatel4687 <ansh40415@gmail.com>
2025-12-23 11:24:35 +0530

    Hello chNAGE

--- chaiurDjango/manage.py
+++ chaiurDjango/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative Hello"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaiurDjango.settings')
     try:
         from django.core.management import execute_from_command_line
